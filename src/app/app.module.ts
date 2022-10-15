8c72eaa346f52305021288b1368a83acc758af1c
ALIABDO111 <Aboalhassanabdo769@gmail.com>
2022-10-15 15:42:10 +0200

    layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { NavparComponent } from './navpar/navpar.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavparComponent,
-    FooterComponent,
     NotfoundComponent,
   ],
   imports: [
