2446683fed121604500c13ab39e78664fe17912f
TahaAhmedd <teto67523@gmail.com>
2022-11-20 20:00:16 +0200

    finish book and add tostify

--- src/app/Model/ApiResponse.ts
+++ src/app/Model/ApiResponse.ts
@@ -1,6 +1,33 @@
 export class ApiResponse{
-    data=[];
+    data:[]=[];
     success="";
     message=true;
     status=true;
-} 
+}
+
+
+export interface DataBookResult{
+    _id:number;
+    title:string;
+    description:string;
+    categoryName:string;
+    keywords:[];
+    createdAt:string;
+    updatedAt:string;
+    cover:string;
+    link:string
+}
+
+// {
+//     "_id": "63793c52ee436ce8f3bf5290",
+//     "title": "Test",
+//     "description": "Test Des",
+//     "categoryName": "test1",
+//     "keywords": [
+//         "test"
+//     ],
+//     "createdAt": "2022-11-19T20:28:03.042Z",
+//     "updatedAt": "2022-11-19T20:28:03.114Z",
+//     "cover": "http://localhost:4000/images/blaz-photo-zMRLZh40kms-unsplash.png",
+//     "link": "http://localhost:4000/files/لوجو صنايعي3.pdf"
+// }
--- src/app/services/books/book.service.ts
+++ src/app/services/books/book.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { ApiResponse } from 'src/app/Model/ApiResponse';
+import { environment } from 'src/environments/environment';
 import {Bookes} from '../../Model/Bookes'
 
 @Injectable({
@@ -8,57 +11,66 @@ import {Bookes} from '../../Model/Bookes'
 })
 export class BookService {
   // Insert Link Api Inside String
-  allBooke :Bookes |any=[]
-  private url = 'http://localhost:4000/book/all';
+  // allBooke :Bookes |any=[]
+  // private url = 'http://localhost:4000/book/all';
 
   constructor(private http: HttpClient) {}
 
   // Function Get All Books From Api
-  getAllBooks() {
-    console.log('hell');
-    this.http
-      .get<{[key:string]:Bookes}>('http://localhost:4000/book/all')
-      .pipe(
-        map((res) => {
-          const bookes = []
-          // for(const key in res){
-            // console.log({res});
-          const  {data ,message,success}=res
-          // bookes.push({...res})
+//   getAllBooks() {
+//     console.log('hell');
+//     this.http
+//       .get<{[key:string]:Bookes}>('http://localhost:4000/book/all')
+//       .pipe(
+//         map((res) => {
+//           const bookes = []
+//           // for(const key in res){
+//             // console.log({res});
+//           const  {data ,message,success}=res
+//           // bookes.push({...res})
           
           
-          return data
+//           return data
 
-        })
-      )
-.subscribe((data) => {
-        this.allBooke=data
-        console.log(this.allBooke);
+//         })
+//       )
+// .subscribe((data) => {
+//         this.allBooke=data
+//         console.log(this.allBooke);
         
-      });
+//       });
+//   }
+
+  getAll():Observable<ApiResponse>{
+    return this.http.get<ApiResponse>(`${environment.PathApi}/book/all`);
   }
   // Function Get Book By Id Api
-  getBookByID() {
-    return this.http.get(this.url);
+  getBookByID(Id:number) {
+    console.log(Id)
+    return this.http.get(`${environment.PathApi}/book/books/${Id}`);
   }
+
+
   // Function Get Book By CaticoryFrom Api
-  getBookByCaticory(category: any) {
-    return this.http.get(this.url);
+  getBookByCatigory(category: any) {
+    return this.http.get(`${environment.PathApi}/book/books/${category}`);
   }
+
+
   // Function Add Book From Api
   postBook(book: any) {
-    return this.http
-      .post('http://localhost:4000/book/newbook', book)
-      .subscribe((res) => {
-        console.log(res);
-      });
+    return this.http.post(`${environment.PathApi}/book/newbook`, book);
   }
+
+
   // Function Delte b Book
-  deletBook() {
-    return this.http.delete(this.url, {});
+  deletBook(id : number) {
+    return this.http.delete(`${environment.PathApi}/book/delete/${id}`);
   }
+
+
   // Function Edit  Book From Api
-  editBook() {
-    return this.http.put(this.url, {});
+  editBook(id:number , data:any) {
+    return this.http.put(`${environment.PathApi}/book/update/${id}`,data);
   }
 }
