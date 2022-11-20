fd7b2316256913ab40bed85c3910c527ec7ca1fa
mostafahpater <[mostafahpater@gamil.com>
2022-11-20 21:54:12 +0200

    edit in ui

--- src/app/user/categories/categories.component.ts
+++ src/app/user/categories/categories.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
-
+catogry:any[]=["ahmed","ali","mohamed"]
+setlocalstorge(value){
+  localStorage.setItem("catogry",value)
+}
   constructor() { }
 
   ngOnInit(): void {
