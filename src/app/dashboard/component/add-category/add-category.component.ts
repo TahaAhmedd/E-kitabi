1f66f376351808f3b96f0ead0c02a8bde08bb6d6
abdalhafezsayeh <abdalahfezsayeh1997@gmail.com>
2022-11-21 10:41:06 +0200

    fnsh add catBook

--- src/app/dashboard/component/add-category/add-category.component.ts
+++ src/app/dashboard/component/add-category/add-category.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { CatigotyBookService } from 'src/app/services/books/catigoty-book.service';
 
 @Component({
   selector: 'app-add-category',
@@ -6,10 +10,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-category.component.css']
 })
 export class AddCategoryComponent implements OnInit {
-
-  constructor() { }
+  addBook:FormGroup;
+  imagearr: any = [];
+  constructor(private router: Router
+    ,private serve:CatigotyBookService ,
+    private tost: ToastrService
+  ) {
+    this.addBook = new FormGroup({
+      title: new FormControl("", [Validators.required]),
+      categoryImage: new FormControl("", [Validators.required]),
+    })
+  }
 
   ngOnInit(): void {
   }
 
+
+  readURL(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      const files = event.target.files;
+      const file = event.target.files[0]
+      this.addBook.patchValue({
+        categoryImage:file
+      })
+      for (var i = 0; i < files.length; i++) {
+        const reader = new FileReader();
+        reader.onload = e => this.imagearr.push(e.target!.result);
+        reader.readAsDataURL(files[i]);
+
+      }
+    }
+  }
+
+  addBookData() {
+    console.log(this.addBook.value)
+    const formData = new FormData()
+    formData.append("title",this.addBook.get("title").value)
+    formData.append("categoryImage", this.addBook.get("categoryImage").value)
+
+
+    this.serve.postBookCat(formData).subscribe({
+      next:(value)=> {
+        console.log(value);
+        this.tost.success("The Book is Added Succesfuly","",{
+          positionClass:"toast-bottom-right",
+          progressBar:true,
+          
+        })
+        // location.reload()
+      },
+      error(err) {
+        console.log(err)
+      },
+    })
+  }
 }
--- src/app/user/categories/categories.component.ts
+++ src/app/user/categories/categories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CatigotyBookService } from 'src/app/services/books/catigoty-book.service';
 
 @Component({
   selector: 'app-categories',
@@ -6,13 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
-// catogry:any[]=[]
+catogry:any
 // setlocalstorge(value){
 //   localStorage.setItem("catogry",value)
 // }
-  constructor() { }
+  constructor(private serBookCat:CatigotyBookService) { }
 
   ngOnInit(): void {
+
+    // Get All Books Art
+    this.serBookCat.getAllBookCat().subscribe((response) => {
+      this.catogry = response.data
+      console.log(this.catogry)
+    })
+
+
   }
 
 }
+
+
+
