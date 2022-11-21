36c508338be8f41015a1d219d9b3d4cfe8461253
TahaAhmedd <teto67523@gmail.com>
2022-11-22 00:48:55 +0200

    Add Articel

--- src/app/services/books/book.service.ts
+++ src/app/services/books/book.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ApiResponse } from 'src/app/Model/ApiResponse';
-
 import {Bookes} from '../../Model/Bookes'
 import { environment } from './../../../environments/environment';
 
@@ -11,52 +10,22 @@ import { environment } from './../../../environments/environment';
   providedIn: 'root',
 })
 export class BookService {
-  // Insert Link Api Inside String
-  // allBooke :Bookes |any=[]
-  // private url = 'http://localhost:4000/book/all';
-
   constructor(private http: HttpClient) {}
-
-  // Function Get All Books From Api
-//   getAllBooks() {
-//     console.log('hell');
-//     this.http
-//       .get<{[key:string]:Bookes}>('http://localhost:4000/book/all')
-//       .pipe(
-//         map((res) => {
-//           const bookes = []
-//           // for(const key in res){
-//             // console.log({res});
-//           const  {data ,message,success}=res
-//           // bookes.push({...res})
-          
-          
-//           return data
-
-//         })
-//       )
-// .subscribe((data) => {
-//         this.allBooke=data
-//         console.log(this.allBooke);
-        
-//       });
-//   }
 
   getAll():Observable<ApiResponse>{
     return this.http.get<ApiResponse>(`${environment.PathApi}/book/all`);
   }
+
   // Function Get Book By Id Api
   getBookByID(Id:number) {
     console.log(Id)
-    return this.http.get(`${environment.PathApi}/book/books/${Id}`);
+    return this.http.get(`${environment.PathApi}/book/getbyid/${Id}`);
   }
-
-
+  
   // Function Get Book By CaticoryFrom Api
   getBookByCatigory(category: any) {
     return this.http.get(`${environment.PathApi}/book/books/${category}`);
   }
-
 
   // Function Add Book From Api
   postBook(book: any) {
