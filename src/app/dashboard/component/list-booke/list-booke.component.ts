27adbaeb768789862bb66305d914d8e4c4348087
ALIABDO111 <Aboalhassanabdo769@gmail.com>
2022-11-22 16:51:39 +0200

    hello  TAha Polymorphism

--- src/app/dashboard/component/list-booke/list-booke.component.ts
+++ src/app/dashboard/component/list-booke/list-booke.component.ts
@@ -2,67 +2,88 @@ import { getLocaleDateFormat } from '@angular/common';
 import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
+import { debounce, debounceTime, distinct, distinctUntilChanged } from 'rxjs';
 import { DataBookResult } from 'src/app/Model/ApiResponse';
 import { BookService } from 'src/app/services/books/book.service';
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-list-booke',
   templateUrl: './list-booke.component.html',
-  styleUrls: ['./list-booke.component.css']
+  styleUrls: ['./list-booke.component.css'],
 })
 export class ListBookeComponent implements OnInit, OnChanges {
-  arrBook: DataBookResult[]
+  arrBook: DataBookResult[];
+
+  //search
+  formsearch: FormGroup = new FormGroup({
+    search: new FormControl(''),
+  });
+
   addBook!: FormGroup;
   imagearr: any = [];
   constructor(private httpServes: BookService, private toast: ToastrService) {
     this.addBook = new FormGroup({
-      title: new FormControl("", [Validators.required]),
-      description: new FormControl("", [Validators.required]),
-      bookImage: new FormControl("", [Validators.required]),
-      bookFile: new FormControl("", [Validators.required]),
-      keywords: new FormControl("", [Validators.required]),
-      categoryName: new FormControl("test", [Validators.required]),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      bookImage: new FormControl('', [Validators.required]),
+      bookFile: new FormControl('', [Validators.required]),
+      keywords: new FormControl('', [Validators.required]),
+      categoryName: new FormControl('test', [Validators.required]),
       fileSource: new FormControl(null),
-      imageSource: new FormControl(null)
-    })
+      imageSource: new FormControl(null),
+    });
+
+    //search
+
+  this.formsearch
+
+      .get('search')
+      .valueChanges.pipe(
+        debounceTime(1000),
+        distinctUntilChanged(),
+        switchMap((item) => this.httpServes.searchBooke(item))
+      )
+      .subscribe((v) => {
+        console.log(v.data);
+        this.arrBook = v?.data;
+      });
   }
-  ngOnChanges(): void {
-  }
+  ngOnChanges(): void {}
 
   ngOnInit(): void {
-    this.getdata()
+    this.getdata();
   }
   getdata() {
     return this.httpServes.getAll().subscribe((e) => {
-      this.arrBook = e.data
-    })
+      this.arrBook = e.data;
+    });
   }
   deletBook(id: number) {
     this.httpServes.deletBook(id).subscribe({
       next: () => {
         // this.tostSucces("The Book is Deleted Succesfuly")
-        this.toast.success("The Book is Deleted Succesfuly")
-        this.getdata()
+        this.toast.success('The Book is Deleted Succesfuly');
+        this.getdata();
       },
-      error:(err)=> {
-        this.toast.error("An error occurred, please try again")
+      error: (err) => {
+        this.toast.error('An error occurred, please try again');
       },
-    })
+    });
   }
 
   // image On Change
   readURL(event: any) {
     if (event.target.files && event.target.files[0]) {
       const files = event.target.files;
-      const file = event.target.files[0]
+      const file = event.target.files[0];
       this.addBook.patchValue({
-        imageSource: file
-      })
+        imageSource: file,
+      });
       for (var i = 0; i < files.length; i++) {
         const reader = new FileReader();
-        reader.onload = e => this.imagearr.push(e.target!.result);
+        reader.onload = (e) => this.imagearr.push(e.target!.result);
         reader.readAsDataURL(files[i]);
-
       }
     }
   }
@@ -72,32 +93,29 @@ export class ListBookeComponent implements OnInit, OnChanges {
     if (e.target.files.length > 0) {
       const file = e.target.files[0];
       this.addBook.patchValue({
-        fileSource: file
-      })
+        fileSource: file,
+      });
     }
   }
 
   updateBook(id: number) {
-    const formData = new FormData()
-    formData.append("title", this.addBook.get("title").value)
-    formData.append("description", this.addBook.get("description").value)
-    formData.append("categoryName", "test")
-    formData.append("keywords", "")
-    formData.append("bookFile", this.addBook.get("fileSource").value)
-    formData.append("bookImage", this.addBook.get("imageSource").value)
+    const formData = new FormData();
+    formData.append('title', this.addBook.get('title').value);
+    formData.append('description', this.addBook.get('description').value);
+    formData.append('categoryName', 'test');
+    formData.append('keywords', '');
+    formData.append('bookFile', this.addBook.get('fileSource').value);
+    formData.append('bookImage', this.addBook.get('imageSource').value);
     this.httpServes.editBook(id, formData).subscribe({
       next: (value) => {
         // console.log(value)
-        this.toast.success("The Book is Deleted Succesfuly")
-        this.getdata()
+        this.toast.success('The Book is Deleted Succesfuly');
+        this.getdata();
       },
-      error:(err)=> {
-        this.toast.error("An error occurred, please try again")
+      error: (err) => {
+        this.toast.error('An error occurred, please try again');
       },
-    })
+    });
   }
-
-  searchBook() {
-    
-  }
+  //////////////search ///////////////
 }
--- src/app/services/books/book.service.ts
+++ src/app/services/books/book.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable, observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ApiResponse } from 'src/app/Model/ApiResponse';
-import {Bookes} from '../../Model/Bookes'
+import { Bookes } from '../../Model/Bookes';
 import { environment } from './../../../environments/environment';
 
 @Injectable({
@@ -12,24 +12,28 @@ import { environment } from './../../../environments/environment';
 export class BookService {
   constructor(private http: HttpClient) {}
 
-  getAll():Observable<ApiResponse>{
+  getAll(): Observable<ApiResponse> {
     return this.http.get<ApiResponse>(`${environment.PathApi}/book/all`);
   }
 
   // Function Get Book By Id Api
-  getBookByID(Id:String) {
-    console.log(Id)
-    return this.http.get<ApiResponse>(`${environment.PathApi}/book/getbyid/${Id}`);
+  getBookByID(Id: String) {
+    console.log(Id);
+    return this.http.get<ApiResponse>(
+      `${environment.PathApi}/book/getbyid/${Id}`
+    );
   }
-  
+
   // Function Get Book By CaticoryFrom Api
-  getBookByCatigory(category: any):Observable<ApiResponse> {
+  getBookByCatigory(category: any): Observable<ApiResponse> {
     console.log(category);
-    return this.http.get<ApiResponse>(`${environment.PathApi}/book/books/${category}`);
+    return this.http.get<ApiResponse>(
+      `${environment.PathApi}/book/books/${category}`
+    );
   }
   getBookByTitle(title: string) {
     console.log(title);
-    
+
     return this.http.get(`${environment.PathApi}/book/books/${title}`);
   }
 
@@ -38,15 +42,17 @@ export class BookService {
     return this.http.post(`${environment.PathApi}/book/newbook`, book);
   }
 
-
   // Function Delte b Book
-  deletBook(id : number) {
+  deletBook(id: number) {
     return this.http.delete(`${environment.PathApi}/book/delete/${id}`);
   }
 
-
   // Function Edit  Book From Api
-  editBook(id:number , data:any) {
-    return this.http.put(`${environment.PathApi}/book/update/${id}`,data);
+  editBook(id: number, data: any) {
+    return this.http.put(`${environment.PathApi}/book/update/${id}`, data);
+  }
+  //Function Search about Book
+  searchBooke(search: string):Observable<ApiResponse> {
+    return this.http.get<ApiResponse>(`${environment.PathApi}/book/search/${search}`);
   }
 }
