4d19fb6a69286a4cc1ce0004033a1629b9c3b4ea
TahaAhmedd <teto67523@gmail.com>
2022-12-18 16:42:26 +0200

    Fix Some bugs in App

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,7 +7,6 @@ import {
   Validators,
 } from '@angular/forms';
 import { Router } from '@angular/router';
-import { async } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-login',
@@ -28,40 +27,41 @@ export class LoginComponent implements OnInit {
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required]),
     });
-    localStorage.removeItem("token")
+    localStorage.removeItem("toomvdvdsvdvken")
     localStorage.removeItem("id")
   }
   add() {
 
     // console.log(this.loginForm.value)
-    if (this.loginForm.valid) {
-      this.Authserver.login(this.loginForm.value).subscribe({
-        next: (result) => {
-          // console.log(result);        
-          this.token=result.data.token
-          let id =result.data._id
-          localStorage.setItem('id', id)
-          localStorage.setItem('token', this.token )   
-        },
-        error:(err)=>{
-          // console.log(err)
-          this.toast.error("The Email Or Password Is Not Valid","Error")
+    // if (this.loginForm.valid) {
+    //   this.Authserver.login(this.loginForm.value).subscribe({
+    //     next: (result) => {
+    //       // console.log(result);        
+    //       this.token=result.data.token
+    //       let id =result.data._id
+    //       localStorage.setItem('id', id)
+    //       localStorage.setItem(`${this.token}`, this.token )   
+    //     },
+    //     error:(err)=>{
+    //       // console.log(err)
+    //       this.toast.error("The Email Or Password Is Not Valid","Error")
         
-        },
-        complete:()=>{
-          let token =  localStorage.getItem("token")
-          if(localStorage.getItem('token')){
-            if(token == this.token){
-              this.router.navigate(['dashboard/card'])
-              this.Authserver.isloginuser.next(true)
-            }
-          }
-          else{
-            this.Authserver.isloginuser.next(false)
-          }
-        }
+    //     },
+    //     complete:()=>{
+    //       let token =  localStorage.getItem(this.token)
+    //       if(localStorage.getItem(this.token)){
+    //         if(token == this.token){
+    //           this.router.navigate(['dashboard/card'])
+    //           this.Authserver.isloginuser.next(true)
+    //         }
+    //       }
+    //       else{
+    //         this.Authserver.isloginuser.next(false)
+    //       }
+    //     }
 
-      });
-    }
+    //   });
+    // }
+    this.Authserver.loged(this.loginForm.value)
   }
 }
